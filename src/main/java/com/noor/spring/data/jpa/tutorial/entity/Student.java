443542d80500109533c735d5b894c104c21518ea
Gurnoor Deol <gurnoor.deol@forgeahead.io>
2022-12-21 21:27:00 -0800

    Adding Student Entity and establishing connection to the database.

---
+++ src/main/java/com/noor/spring/data/jpa/tutorial/entity/Student.java
@@ -0,0 +1,27 @@
+package com.noor.spring.data.jpa.tutorial.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Long studentId;
+
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
